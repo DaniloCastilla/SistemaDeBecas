634d95ec1cb2a7e78806e1efdd8b21e272841fb3
sergiocada <33910635+sergiocada@users.noreply.github.com>
2019-11-29 09:52:47 -0500

    Update usuar.sql

--- usuar.sql
+++ usuar.sql
@@ -1,33 +1,19 @@
-create database ingdatabase;
-use ingdatabase;
-create table rol(
-	id_rol int(11) auto_increment,
-	rol varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
-	primary key (id_rol)
+create database ProyectoBecas;
+use ProyectoBecas;
+create table Becas(
+	TipodeBeca int(11) auto_increment,
+	Beca varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+	primary key (TipodeBeca)
 );
-create table usuar(
+create table InformacionUsuario(
 	id_user int(11) auto_increment,
-	username varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
-	contraseña varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
-	email varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
-	nombre varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
-	apellido varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
-	id_rol int(11),
+    correo varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+   	documento varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+    nombre varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+    apellido varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+    Direccion	varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
+    TipodeBeca int(11),
+
 	primary key (id_user),
-	foreign key (id_rol) references rol(id_rol)
-);
-create table Documentos(
-    id_documento int(11) auto_increment,
-    titulo varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
-    autor varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
-    primary key(id_documento)    
-);
-create table metadatos(
-	id_metadato int(11) auto_increment,
-	tamaño int(11),
-	tipe varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
- 	ruta varchar(150) COLLATE utf8_spanish_ci DEFAULT NULL,
- 	id_doc int(11),
-	primary key(id_metadato),  
-	foreign key (id_doc) references Documentos(id_documento)
+	foreign key (TipodeBeca) references Becas(TipodeBeca)
 );
